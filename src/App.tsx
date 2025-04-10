27c8416abbdf9b4240989c6b108314640f0ecfaf
CedricJianN <cedricjianmartin@gmail.com>
2025-04-10 23:38:19 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Personality } from './types.ts';
+
+export default function Gallery() {
+  const [personalities, setPersonalities] = useState<Personality[]>([]);
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/martin/personalities')
+      .then(res => res.json())
+      .then(data => setPersonalities(data));
+  }, []);
+
+  if (personalities.length === 0) return <p>No data</p>;
+
+  const hasNext = index < personalities.length - 1;
+  const hasBack = index > 0;
+  const personality = personalities[index];
+
+  return (
+    <>
+      <h2>Cedric Jian N. Martin</h2>
+      <button onClick={() => setIndex(hasBack ? index - 1 : personalities.length - 1)}>Back</button>
+      <button onClick={() => setIndex(hasNext ? index + 1 : 0)}>Next</button>
+      <h2><i>{personality.name}</i></h2>
+      <h3>({index + 1} of {personalities.length})</h3>
+      <button onClick={() => setShowMore(!showMore)}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{personality.description}</p>}
+      <img src={personality.url} alt={personality.alt} />
+    </>
+  );
+}
---
+++ src/data.tsx
@@ -0,0 +1,85 @@
+export const sculptureList = [{
+    "name": "Re:Zero",
+    "artist": "Shinichirō Ōtsuka",
+    "description": "A NEET gamer, Subaru, is suddenly transported to a fantasy world and meets a silver-haired half-elf girl.",
+    "url": "https://www.animenewsnetwork.com/images/encyc/A17360-3211712697.1460348209.jpg",
+    "alt": "Subaru Natsuki is a NEET who is suddenly summoned to a fantasy-like world."
+  },
+  {
+    "name": "Dead Mount Death Play",
+    "artist": "Ryohgo Narita",
+    "description": "A necromancer escapes death by reincarnating into modern Shinjuku, taking over a boy's body and blending into the underworld.",
+    "url": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx157198-LVZ7PgxWwc6v.jpg",
+    "alt": "Shagrua Edith Lugrid is about to finish off a powerful necromancer known as the Corpse God."
+  },
+  {
+    "name": "KonoSuba",
+    "artist": "Natsume Akatsuki",
+    "description": "After dying, shut-in Kazuma is reincarnated into a fantasy world and drags a useless goddess along with him.",
+    "url": "https://i.pinimg.com/736x/61/01/c9/6101c98a723e73e161913b1c784fcd22.jpg",
+    "alt": " Kazuma Satou, a Japanese teenage shut-in NEET."
+  },
+  {
+    "name": "KonoSuba: An Explosion on This Wonderful World!",
+    "artist": "Natsume Akatsuki",
+    "description": "A spin-off focusing on Megumin’s life before joining Kazuma's party.",
+    "url": "https://m.media-amazon.com/images/M/MV5BYTRiM2VmNjgtZWY5ZS00YTIxLWJhMjAtY2UzYTk3NzM5ZTM2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "alt": "Three monumental stone busts with the heads that are disproportionately large with somber faces."
+  },
+  {
+    "name": "Attack on Titan",
+    "artist": "Hajime Isayama",
+    "description": "Humanity battles for survival against towering man-eating Titans.",
+    "url": "https://m.media-amazon.com/images/M/MV5BZTAyMjUxYWItYTBkNS00YmNlLWEzNmItMDQ0MTRlYzQwNzIyXkEyXkFqcGc@._V1_.jpg",
+    "alt": ""
+  },
+  {
+    "name": "Death Note",
+    "artist": "Tsugumi Ohba, Takeshi Obata",
+    "description": "A student gains a notebook that kills anyone whose name is written in it.",
+    "url": "https://m.media-amazon.com/images/M/MV5BYTgyZDhmMTEtZDFhNi00MTc4LTg3NjUtYWJlNGE5Mzk2NzMxXkEyXkFqcGc@._V1_.jpg",
+    "alt": ""
+  },
+  {
+    "name": "One Piece",
+    "artist": "Eiichiro Oda",
+    "description": "Luffy and his crew search for the legendary treasure, One Piece.",
+    "url": "https://m.media-amazon.com/images/M/MV5BMTNjNGU4NTUtYmVjMy00YjRiLTkxMWUtNzZkMDNiYjZhNmViXkEyXkFqcGc@._V1_.jpg",
+    "alt": ""
+  },
+  {
+    "name": "Naruto",
+    "artist": "Masashi Kishimoto",
+    "description": "A young ninja strives to become his village’s strongest leader, the Hokage.",
+    "url": "https://www.manga-news.com/public/images/dvd/.naruto-visuel-anime_large.webp",
+    "alt": ""
+  },
+  {
+    "name": "Demon Slayer",
+    "artist": "Koyoharu Gotouge",
+    "description": "A boy joins the Demon Slayer Corps to avenge his family and save his sister.",
+    "url": "https://www.orientaserie.it/wp-content/uploads/2022/09/EV4zrsBUMAI1hBY.jpg",
+    "alt": ""
+  },
+  {
+    "name": "Steins;Gate",
+    "artist": "5pb., Nitroplus",
+    "description": "A mad scientist stumbles upon time travel with dangerous consequences.",
+    "url": "https://m.media-amazon.com/images/M/MV5BZjI1YjZiMDUtZTI3MC00YTA5LWIzMmMtZmQ0NTZiYWM4NTYwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "alt": ""
+  },
+  {
+    "name": "My Hero Academia",
+    "artist": "Kohei Horikoshi",
+    "description": "In a world of superpowers, a powerless boy dreams of becoming a hero.",
+    "url": "https://preview.redd.it/new-key-visual-for-my-hero-academia-the-movie-youre-next-v0-bumpbozqv36d1.jpeg?auto=webp&s=2e73ef614099bedd30105b7b15c1539ec567a562",
+    "alt": ""
+  },
+  {
+    "name": "Fullmetal Alchemist: Brotherhood",
+    "artist": "Hiromu Arakawa",
+    "description": "Two brothers seek the Philosopher’s Stone after a failed alchemy experiment.",
+    "url": "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8042516_b_v8_ab.jpg",
+    "alt": ""
+  }];
+  
